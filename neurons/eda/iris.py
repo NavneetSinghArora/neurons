6db89f7bc0febee9c58a195b0e1317577e1bbd2b
Navneet Singh Arora <navneet@repath.earth>
2025-02-12 19:19:38 +0100

    Adding the getter methods for the IRIS Dataset

--- neurons/eda/iris.py
+++ neurons/eda/iris.py
@@ -11,13 +11,21 @@ import numpy as np
 
 from neurons.dataset import get_dataset
 
+
+def get_features(dataset=get_dataset("IRIS")):
+    return dataset.iloc[0:100, [0, 2]].values
+
+
+def get_targets():
+    target = dataset.iloc[0:100, 4].values
+    return np.where(target == "Iris-setosa", 0, 1)
+
 # Select Target Variable (Sentosa and Versicolor Flowers) from the Iris dataset and mark them as 0 and 1
 dataset = get_dataset("IRIS")
-target = dataset.iloc[0:100, 4].values
-target =  np.where(target == 'Iris-setosa', 0, 1)
+target =  get_targets()
 
 # Select the first two features (Sepal Length and Petal Length) for training
-features = dataset.iloc[0:100, [0, 2]].values
+features = get_features()
 
 # Create a Scatter Plot for the data
 plt.scatter(features[:50, 0], features[:50, 1], color='red', marker='o', label='Sentosa')
